5f93f0d585813a38ab2023ada9e9c96045c1ba44
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-08-31 01:13:22 +0530

    Implement Responsive Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomepageComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contactus', component: ContactUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProjectsComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
